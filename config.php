e39b0aed411a41b56734c32e734154adee553c09
techevo <abhisheksiriya@icloud.com>
2014-07-28 01:37:35 +0530

    update sunday

--- config.php
+++ config.php
@@ -1,14 +1,75 @@
 <?php
 
     try {
-        $dbh = new PDO('mysql:host=localhost;dbname=lms;charset=UTF-8', 'root', 'admin');
-		$columns = array('id','account','standard_number','sequence','url','created_on','created_by','modified_on','modified_by');
-		$pdfTable = json_encode('StandardPDF',$columns);
+        $con = mysqli_connect("localhost","root","","test");
+		
+		define("tableName" ,  "standardpdf");
+		define("col1" , "id");//an auto-increment ID
+		define("col2" , "account");//the 6 character identifier of the Account
+		define("col3" , "standard_number");//the numeric identifier of the Standard
+		define("col4" , "sequence");//for now this will always be ‘1’ as it’s a 1-to-1 relationship of PDF-to-Standard but ultimately we’ll allow multiples and this would control order
+		define("col5" , "url");//the URL where the PDF is stored
+		define("col6" , "created_on");//timestamp of when the record was created
+		define("col7" , "created_by");//the ID of the user that created the record ($session_RepID)
+		define("col8" , "modified_on");//timestamp of when the record was last modified
+		define("col9" , "modified_by");//the ID of the user that last modified the record ($session_RepID)
+		
+		
+		
+		//Prepare query string for each CRUD operations
+		function getRetriveQuery($param3) {
+			$retriveQuery = "SELECT * from standardpdf 
+							 WHERE " .col3. " = $param3 
+							 ORDER BY " .col6. " desc limit 1";
+			return $retriveQuery;
+		}
+		
+		function getUpdateQuery($val1,
+								$val2,
+								$val3
+								) {
+			$updateQuery = "UPDATE " .tableName. "
+								SET " .col2. " =$val1,
+									" .col5. " =$val2,
+									" .col9. " =$val3";
+		
+			return $updateQuery;
+		}
+		function getInsertQuery($val2,
+								$val3,
+								$val5,
+								$val7,
+								$val9) {
+		$insertQuery = "INSERT INTO " .tableName. " 
+									(
+									 ".col2.",
+									 ".col3.",
+									 ".col5.",
+									 ".col7.",
+									 ".col9."
+									)
+								VALUES
+									 (
+									 $val2,
+									 $val3,
+									 $val5,
+									 $val7,
+									 $val9
+									 )
+								
+								)";
+			return $insertQuery;
+		}
+		function getDeleteQuery ($param3) {
+			$deleteQuery = "DELETE FROM " .tableName. " 
+							 WHERE ".col3."=$param3";
+		}
     }
     catch (PDOException $e) {
-        die('unable to connect to database ' . $e->getMessage());
+        echo 'ERROR: ' . $e->getMessage();
+		//die('unable to connect to database ' . $e->getMessage());
     }    
     // create LM object, pass in PDO connection
-    $lm = new lazy_mofo($dbh);
+    //$lm = new lazy_mofo($dbh);
 
 ?>
--- upload.php
+++ upload.php
@@ -1,33 +1,91 @@
 <?php
-<<<<<<< HEAD
+$con;
 include("config.php");
-=======
->>>>>>> origin/master
-$targetFolder = '/uploads'; // Relative to the root
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+$targetFolder = 'storepdfs/'; // Relative to the root
+$session_account = "ADV150";
+$session_repID = "DemoRep";
+
+if (!isset($_SESSION["session_account"])) $_SESSION["session_account"]=$session_account;
+if (!isset($_SESSION["session_repID"])) $_SESSION["session_repID"]=$session_repID;
+
+if($_POST['operationtype'] == null && $_POST['ordernumber']!= null){
+	retrivePdfForOrder($_POST['ordernumber']);
+}
+
+if($_POST['operationtype'] == 'add'){
+	if (!empty($_FILES)) {
+		//call addpdf
+		$file_name = addpdf($_POST['ordernumber']);
+		$tempFile = $_FILES['file']['tmp_name'];
+		$targetPath = dirname(__FILE__) . '/' . $targetFolder;
+		$targetFile = rtrim($targetPath,'/') . '/' . $file_name;//$_FILES['file']['name'];
+		// Validate the file type
+		$fileTypes = array('pdf'); // File extensions
+		$fileParts = pathinfo($_FILES['file']['name']);
+		
+		if (in_array($fileParts['extension'],$fileTypes)) {
+			move_uploaded_file($tempFile,$targetFile);
+			echo '1';
+		} else {
+			echo 'Invalid file type.';
+		}
+		
+	}
+}
+
+if($_POST['operationtype'] == 'view'){
+	//retrive file path from data base and set the path
+	//query form the database
+	$result = mysqli_query($con,getRetriveQuery($paramSent));
+	$row = mysqli_fetch_array($result);
+	$filePath = $row->url;
+	$file = $root.$filePath;
+	downloadFile($file);
+}
+if($_POST['operationtype'] == 'delete'){
+	$fileName = $root."/PHPMySQLFileUPload/".$targetFolder."/";
+	//rename the file
+	renameFile($fileName,$root."/FileUploadUpdate/storpdfs/");
+	//delete the entry from the database
+	
+}
+
+
+//insert when clicked on add
+function addpdf($orderNum){
+	$retInfo = retrivePdfForOrder($orderNum);
+	$file_name;
+	if($retInfo == null) {
+		$file_name = $_SESSION["session_account"]."_".$orderNum."_001";
+	} else {
+		$file_name = $_SESSION["session_account"]."_".$orderNum . (substr($retInfo->url,-3) + 1);
+	}
+	global $con;
+	$file_name = $_SESSION["session_account"]."_".$orderNum;
+	$result = mysqli_query($con,getInsertQuery($_SESSION["session_account"],
+											   $orderNum,
+											   $root."/PHPMySQLFileUPload/".$targetFolder."/".$file_name,
+											   $_SESSION["session_repID"],
+											   $_SESSION["session_repID"]
+												));
+	
+	return $file_name;
+}
+
+//onload function
+function retrivePdfForOrder($paramSent){
+	//query form the database
+	global $con;
+	$result = mysqli_query($con,getRetriveQuery($paramSent));
+	$row = mysqli_fetch_array($result);
+	echo json_encode($row);
+	return $row;
+}
+
 
 //view pdf
 //queryfrom the database the file path by using order number and passing the file path 
-if($_POST['operationtype'] == 'view'){
-	$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-	$file = $root.'/PHPMySQLFileUpload/uploads/18072014033237036_1405677757036_XXXPT6161X_ITRV.pdf';
-	downloadFile($file);
-}
-if($_POST['delete']=='delete'){
-	$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-	$fileName = $root."/FileUploadUpdate/uploads/18072014033237036_1405677757036_XXXPT6161X_ITRV.pdf"
-	renameFile($fileName);
-}
-//delete pdf
-//rename file when delete and make the column "file path" value to null and enable add button
-function renameFile($fileName){
-	rename($fileName,$root."/FileUploadUpdate/uploads/_obsolete.pdf");
-	//modify database column for that row.
-}
-
-//change pdf
-//rename the existing pdf by retriving the file path from the database and update the lastmodifieddate and file path columns
-
-
 function downloadFile($file) { // $file = include path 
         if(file_exists($file)) {
             header('Content-Description: File Transfer');
@@ -46,19 +104,10 @@ function downloadFile($file) { // $file = include path
 			echo 'file does not exist';
 		}
 }
-if (!empty($_FILES)) {
-	$tempFile = $_FILES['file']['tmp_name'];
-	$targetPath = dirname(__FILE__) . '/' . $targetFolder;
-	$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['file']['name'];
-	// Validate the file type
-	$fileTypes = array('pdf'); // File extensions
-	$fileParts = pathinfo($_FILES['file']['name']);
-	
-	if (in_array($fileParts['extension'],$fileTypes)) {
-		move_uploaded_file($tempFile,$targetFile);
-		echo '1';
-	} else {
-		echo 'Invalid file type.';
-	}
+//delete pdf
+//rename file when delete and make the column "file path" value to null and enable add button
+function renameFile($fileName){
+	rename($fileName,$root."/FileUploadUpdate/uploads/_obsolete.pdf");
+	//modify database column for that row.
 }
 ?>
