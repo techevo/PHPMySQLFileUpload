22775fa3ce49b49dc3a98f459e1468c387fc12b5
Sharathchandra <sharathchandra.thukkani@gmail.com>
2014-07-30 00:15:16 +0530

    Modified view functionality
    modified view functionality

--- upload.php
+++ upload.php
@@ -94,7 +94,7 @@ function downloadFile($file) { // $file = include path
         if(file_exists($file)) {
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename="IRCTC Ltd,Booked Ticket Printing.pdf"');
+            header('Content-Disposition: attachment; filename="'.basename($file).'"');
             header('Content-Transfer-Encoding: binary');
             header('Expires: 0');
             header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
